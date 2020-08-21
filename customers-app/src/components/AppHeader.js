8124ab71833ad43eb3d59faeb8032816231d20d6
manuonda <manuonda@gmail.com>
2020-08-20 23:09:25 -0300

    init projec

---
+++ customers-app/src/components/AppHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AppHeader = props => {
+    return (
+        <div>
+           Header
+        </div>
+    );
+};
+
+AppHeader.propTypes = {
+    
+};
+
+export default AppHeader;
---
+++ customers-app/src/components/CustomerData.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CustomerData = props => {
+    return (
+        <div>
+            <div className=""></div>
+        </div>
+    );
+};
+
+CustomerData.propTypes = {
+    
+};
+
+export default CustomerData;
