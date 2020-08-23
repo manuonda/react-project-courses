f87fbaa81a4cc8721936256fdaa3b058ed9d9708
dgarcia <manuonda@gmail.com>
2020-08-23 02:05:08 -0300

    se agrega nuevas modificicones por ROute

--- customers-app/src/App.js
+++ customers-app/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Link ,BrowserRouter  as Router } from 'react-router-dom'
+import { Link ,BrowserRouter  as Router , Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class  App  extends React.Component {
+  
+  renderHome = () => <h1> Home </h1>
+  
+  renderCustomerContainer = () => <h1>Customer Container </h1>
+  
+  renderCustomerListContainer = () => <h1> Customers List Container </h1>
+  
+  renderCustomerNewContainer = () => <h1>Customer New Container </h1>
+  
+  
+  render(){
   return (
     <Router>
     <div className="App">
-    <Link to="/customers">Customers</Link>
+     <Route exact path="/" component={ this.renderHome}></Route>
+     <Route exact path="/customers" component={ this.renderCustomerListContainer}></Route>
+     <Route exact path="/customers/new" component={ this.renderCustomerNewContainer}></Route>
+     <Route exact path="/customers/:dni" component={ this.renderCustomerContainer}></Route> 
     </div>
     </Router>
   );
+ } 
+  
 }
 
 export default App;
--- customers-app/src/components/CustomersList.js
+++ customers-app/src/components/CustomersList.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CustomerListItem } from './CustomerListItem'
 
-const CustomersList = props => {
+const CustomersList = ({ customers }) => {
     return (
         <div>
-            
+            <div className="customers-list">
+                {
+                   customers.map( c =>
+                    <CustomerListItem 
+                    key={c.dni}
+                    name={c.name}
+                    dni={c.dni}
+                    editAction={'Editar'}
+                    delAction={'Eliminar'}
+                    urlPath={urlPath}
+                    >
+                    </CustomerListItem>
+                    )  
+                }
+            </div>
         </div>
     );
 };
 
 CustomersList.propTypes = {
-    
+  customers : PropTypes.array.isRequired, 
+  urlPath: PropTypes.string.isRequired,
 };
 
 export default CustomersList;
