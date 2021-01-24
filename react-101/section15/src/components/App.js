fe0296acc87d0b2c6bc8657d661731d4d321c09f
dgarcia <manuonda@gmail.com>
2021-01-24 00:19:17 -0300

    init section 15

---
+++ react-101/section15/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import 'normalize.css'
+import List from './List'
+
+const App = () => {
+
+    const getData = () => {
+
+    }
+    useEffect(() => {
+
+    })
+    return (
+        <>
+            <List></List>
+        </>
+    )
+}
+export default App
---
+++ react-101/section15/src/hookcomponents/fetchData.js
@@ -0,0 +1,21 @@
+const { useState } = require("react")
+
+import React, {useState} from 'react'
+
+const fetchData = (url, initialState={}) => {
+
+   const[data, setData] = useState([])
+   const[fetching, setFetching] = useState(true); 
+   const[error, setError] = use
+   
+   const getData = async () => {
+       try {
+           
+       } catch (error) {
+           
+       }
+   } 
+
+}
+
+export default fetchData;
