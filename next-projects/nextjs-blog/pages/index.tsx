76ec755e57e85ece34cf803737eced1bbbd06fe3
david garcia <manuonda@gmail.com>
2021-04-12 20:52:03 -0300

    modificacion de vista

--- next-projects/nextjs-blog/pages/index.tsx
+++ next-projects/nextjs-blog/pages/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState} from 'react'
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
+
+import {Grid, Stack, Text , Button , Link } from "@chakra-ui/react"
 
 // types 
 import { Product } from '././product/types';
@@ -11,15 +13,50 @@ interface Props {
   products: Product[]
 }
 
-const IndexRoute: React.FC<Props> = ({ children}) => {
-  return <div>IndexRoutes</div>
+
+
+const IndexRoute: React.FC<Props> = ({ products }) => {
+  const[cart, setCart] = useState<Product[]>([]);
+  const text =React.useMemo(() =>{
+    return cart.reduce((message, product) => message.concat(`* ${product.title} - $${product.price}\n`)
+    , ``).concat(`\nTotal : ${cart.reduce((total, product) => total + product.price, 0)}`);
+  }, [cart]); 
+
+
+  const handleAddToCart = (product: Product) => {
+     setCart((cart) => cart.concat(product));
+  }
+  return (
+        <Stack>
+           <Grid gridGap="6" templateColumns="repeat(auto-fill, minmax(240px, 1fr)">
+           { products.map(product => 
+              ( <Stack background="gray.100" key={product.id}>
+                <Text>{product.title}</Text>
+                <Button onClick={() => handleAddToCart(product)} colorScheme="blue">Agregar</Button>
+                </Stack>
+              )
+            )}       
+           </Grid>
+        {Boolean(cart.length) && (
+             <Button 
+                  isExternal
+                  as={Link} 
+                  colorScheme="whatsapp"
+                  href={`https://wa.me/+5493885733589?text=${encodeURIComponent(text)}`}> 
+                  Completar Pedido ({cart.length})
+            </Button>
+           
+        )}
+        </Stack>
+  )
 }
 
+// Props para pasar al componente 
 export const getStaticProps: GetStaticProps = async() => {
   const products = await api.list();
    return{
        props: {
-         products:[]
+         products: products
        }
    }
 }
--- next-projects/nextjs-blog/pages/product/api.ts
+++ next-projects/nextjs-blog/pages/product/api.ts
@@ -1,14 +1,25 @@
 import { Product } from "./types";
-
+import Papa from "papaparse"
 import axios from "axios";
 
 export default {
     list: async (): Promise<Product[]> => {
-        return axios.get(``,{
+        return axios.get(`https://docs.google.com/spreadsheets/d/e/2PACX-1vS7ReWfG9X-1cIeonImC8n5_N9bBYUNEDYw3ZReWuaMrEaoRtKihhg8anlU7bBG8HcJdjUTX6eo2Sld/pub?output=csv`,{
             responseType: 'blob'
         }).then( response => {
-            console.log(response.data);
-            return response.data;
+           return new Promise<Product[]>((resolve, reject) => {
+               Papa.parse(response.data, {
+                   header:true,
+                   complete: results => {
+                       const products = results.data as Product[];
+                      return resolve(products.map(product => ({
+                         ...product,
+                         price :Number(product.price)  
+                      })))
+                   },
+                   error: (error) =>  reject(error.message),
+               });
+           })
         })
     }
 }
