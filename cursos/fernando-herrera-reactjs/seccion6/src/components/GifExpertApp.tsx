b97b57d134b0874065644f8795ddaf4a8399a05f
dgarcia <manuonda@gmail.com>
2021-04-20 16:43:30 -0300

    init project

---
+++ cursos/fernando-herrera-reactjs/seccion6/src/components/GifExpertApp.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GifExpertApp = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+GifExpertApp.propTypes = {
+
+}
+
+export default GifExpertApp
