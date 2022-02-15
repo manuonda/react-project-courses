4d5abf41ce5af0de7d97dfbe0d0534de218e5cb9
dgarcia <manuonda@gmail.com>
2022-02-14 23:12:30 -0300

    init seccion14

---
+++ nicolas-schurman/seccion14/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const  App = () =>  {
+  return (
+    <div className="App">
+      <Container>
+        
+      </Container>
+      
+    </div>
+  );
+}
+
+export default App;
