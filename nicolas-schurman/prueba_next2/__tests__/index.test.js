93762b954fffad8ce8b7f61568b43eda7b2e0c34
dgarcia <manuonda@gmail.com>
2022-05-04 21:35:43 -0300

    modfiicacion de getstaticprops

--- nicolas-schurman/prueba_next2/__tests__/index.test.js
+++ nicolas-schurman/prueba_next2/__tests__/index.test.js
@@ -1,5 +1,5 @@
 import { render, screen} from '@testing-library/react'
-import Index from '../pages/index';
+import Index, {getStaticProps} from '../pages/index';
 
 
 describe('Index',() => {
@@ -9,18 +9,24 @@ describe('Index',() => {
          
           // solamente el componente
           const { getByTestId } =  render(
-            <Index pokemones={[]}></Index>
+            <Index pokemones={[{ name:'Chanchito Feliz', url:'/pokemon/detalle/1'}]}></Index>
          );
 
 
           // elementos dentro de mi documento todo el documento
           const paragraph =  screen.getByText('Mi App Pokemonoes');
-          console.log(paragraph)
+          //console.log(paragraph)
           expect(paragraph).toBeInTheDocument();
 
           const paragraphById = getByTestId('titulo');
-          console.log(paragraphById)
+          //console.log(paragraphById)
        
+          // Dinamicamente se renderiz el componente y establece el nombre como 'Chanchito Feliz'
+          const chanchito  = screen.getByText('Chanchito Feliz');
+          expect(chanchito).toBeInTheDocument();
+          //console.log(chanchito.getAttribute('href'));
+          const url  = chanchito.getAttribute('href');
+          expect(url).toEqual('/pokemons/1');
         
         })
     });
@@ -30,5 +36,24 @@ describe('Index',() => {
          
         });
     })
+
+    describe('getStaticProps', () => {
+       it('return de pokemons', async () =>{
+            // jest.fn() crea una function que tendra un comporamiento definido
+        // en este caso se usara para fetch
+        global.fetch = jest.fn()
+        .mockImplementation( url => {
+            return new Promise(resolve => {
+                resolve({
+                    json:() => Promise.resolve({
+                        results: 'lista de pokemones'
+                    })
+                })
+            })
+        });
+        const { props} = await getStaticProps();
+        expect(props.pokemones).toBe('lista de pokemones');
+       })
+    });
 })
 
