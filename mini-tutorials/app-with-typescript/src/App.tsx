9f18291ea2827071a3d3768c1fa50bd206df71c7
dgarcia <manuonda@gmail.com>
2022-05-06 12:57:03 -0300

    Init project source

--- mini-tutorials/app-with-typescript/src/App.tsx
+++ mini-tutorials/app-with-typescript/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState} from "react";
+
+interface Sub {
+  nick:string,
+  avatar: string,
+  subMonths: number,
+  description?:string
+}
+
+const initialState = [
+  
+]
 
 function App() {
+  const [number, setNumber] = useState<number | string >(5); //number o string 
+  const changeNumber = () => setNumber("3");
+  const[ subs, setSubs] = useState([
+     {
+       nick: 'dadepelu',
+       subMonts: 3,
+       avatar: '',
+       description: 'Informacion numero 1'
+     },{
+       nick: 'sergio_numero',
+       subMonts: 4,
+       avatar:'http://i.pravatar'
+     }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ul>
+         {
+           subs.map( sub => {
+             return (<>
+               <li key={sub.nick}>
+                 <img src={sub.avatar} alt={`Avatar for ${sub.nick}`}>
+                 </img>
+                 <h4>{sub.nick} (<small>{ sub.subMonts}</small>)</h4>
+                 <p>{sub.description?.substring(0,100)}</p>
+               </li>
+             </>)
+           })
+         }
+       </ul>
+       <button onClick={changeNumber}>Change Numer</button>
     </div>
   );
 }
