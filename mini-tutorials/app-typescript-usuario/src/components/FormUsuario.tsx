a3e5a1c703e219ea3c3d0c0e854ecd5966b6dcf1
dgarcia <manuonda@gmail.com>
2022-05-20 08:25:21 -0300

    mini-projects  I

--- mini-tutorials/app-typescript-usuario/src/components/FormUsuario.tsx
+++ mini-tutorials/app-typescript-usuario/src/components/FormUsuario.tsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, {Dispatch, useReducer} from 'react'
+import useReducerUsuarioForm from '../hookReducer/formReducer';
+import useNewReducerUsuarioForm from '../hookReducer/formReducer';
+import FormReducer from '../hookReducer/formReducer';
+import { Usuario } from '../types';
 
-export const FormUsuario = () => {
+
+
+interface Props {
+  onNewUsuario :(usuario: Usuario) => void
+}
+
+
+
+export const FormUsuario = ( { onNewUsuario} : Props) => {
+  
+
+
+  const[ inputValues, dispatch] = useReducerUsuarioForm();
+   
+  console.log('inputValue : ', inputValues)
+  console.log('dispatch : ', dispatch)
+
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const { name , value } = ev.target;
+    dispatch({
+      type: 'change_value',
+      payload: {
+        inputName:name,
+        inputValue: value
+      }
+    });
+  }
+  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+     ev.preventDefault();
+     onNewUsuario(inputValues);
+     handleClear();
+    
+  }
+
+  // AQUI TERMINAR
+
+  const handleClear= () => {
+    dispatch({ type:'clear'});
+  }
+
+
+
+  
   return (
-    <div>FormUsuario</div>
+    <>
+      <div className="">
+       <form onSubmit={handleSubmit}>
+       <label>Nombre</label>
+        <input type="text" name="nombre"  onChange={handleChange} value={inputValues.nombre}/>
+        <label>Apellido</label> 
+        <input type="text" name="apellido"  onChange={handleChange} value={inputValues.apellido}></input>
+        <label>Documento</label>
+        <input type="text" name="documento" onChange={handleChange} value={inputValues.documento}></input>
+      
+        <button type='submit'>Enviar</button>
+        <button type="button" onClick={handleClear}>Limpiar</button>
+       </form>
+      </div>
+    </>
   )
 }
 
