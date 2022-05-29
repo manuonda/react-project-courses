f224abba0aeaf43bad93fdd24ae6f6d434c4201c
dgarcia <manuonda@gmail.com>
2022-05-28 23:27:04 -0300

    create tsx

--- mini-tutorials/acortador-url/src/pages/create.tsx
+++ mini-tutorials/acortador-url/src/pages/create.tsx
@@ -1,9 +1,11 @@
+import { useReducer } from "react";
 import CreateForm from "../components/createForm";
 import MainContainer from "../components/mainContainer";
-
+import reducer
 
 const Create = () =>{
-
+   
+    const[ state, dispatch] =useReducer();
 
     return(
         <>
