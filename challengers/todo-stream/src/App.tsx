99a1d08535a5b3e37e9d16de7f56db7a5d534f78
dgarcia <manuonda@gmail.com>
2022-10-18 08:34:01 -0300

    Modificate file

--- challengers/todo-stream/src/App.tsx
+++ challengers/todo-stream/src/App.tsx
@@ -10,63 +10,88 @@ import { NoteCard } from './components/NoteCard';
 function App() {
   const [notes, setNotes] = React.useState<Note[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
-  
 
-  const handleDelete = (id:Note['id']) :void => {
-       setNotes((notes:Note[]) => notes.filter((note:Note) => note.id !== id));
+
+  type NoteModalProps = {
+    note: Partial<Note>
   }
 
-  const handleArchivo = (id:Note['id']):void => {
-    setNotes((notes: Note[]) => 
-      notes.map((note:Note) => {
-        
-         if(note.id !== id) 
-             return note;
+  function NoteModal({ }: NoteModalProps) {
+    return (
+      <section style={{ width: '100vw', height: '100vw', position: 'fixed', top: 0, left: 0 }}
+        className="nes-dialog" id="dialog-default">
+          <div style={{ backgroundColor: "rgba(0,0,0,0.2)"}} />
+        <form method="dialog">
+          <p className="title">Dialog</p>
+          <p>Alert: this is a dialog.</p>
+          <menu className="dialog-menu">
+            <button className="nes-btn">Cancel</button>
+            <button className="nes-btn is-primary">Confirm</button>
+          </menu>
+        </form>
+      </section>
 
-         return {
+    )
+  }
+
+  const handleDelete = (id: Note['id']): void => {
+    setNotes((notes: Note[]) => notes.filter((note: Note) => note.id !== id));
+  }
+
+  const handleArchivo = (id: Note['id']): void => {
+    setNotes((notes: Note[]) =>
+      notes.map((note: Note) => {
+
+        if (note.id !== id)
+          return note;
+
+        return {
           ...note,
           archived: !note.archived
-         };
+        };
       })
     );
   }
 
   useEffect(() => {
-     const cargarData = async() => {
-       try {
-         const resp = await noteService.list();
-         setNotes(resp);
-         setLoading(false);
-       } catch (error) {
-         setLoading(false);
-       }
-     }
-     setTimeout(() => {
-        cargarData();
-     },2000);   
+    const cargarData = async () => {
+      try {
+        const resp = await noteService.list();
+        setNotes(resp);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    }
+    setTimeout(() => {
+      cargarData();
+    }, 2000);
 
-  },[])
+  }, [])
 
   if (loading) {
     return <>Loading...</>
   }
 
   return (
-    <div style={{marginBottom: 24}}>
-      
+    <div style={{ marginBottom: 24 }}>
+
       <h1>Mis Notas</h1>
       <button>Crear Nota</button>
-      <div style={{ display: 'grid', 
-           alignItems:"center",
-           gap: 24,
-           gridTemplateColumns: "repeat(auto-fill,minmax(480px, 1fr))"}}>
-        { notes.map((note:Note) => (
-            <NoteCard onArchive={handleArchivo} 
-                      onDelete={handleDelete}  
-                      key={note.id} note={note}/>
-           ))}
+      <div style={{
+        display: 'grid',
+        alignItems: "center",
+        gap: 24,
+        gridTemplateColumns: "repeat(auto-fill,minmax(480px, 1fr))"
+      }}>
+        {notes.map((note: Note) => (
+          <NoteCard onArchive={handleArchivo}
+            onDelete={handleDelete}
+            key={note.id} note={note} />
+        ))}
       </div>
-      </div>
+      <NoteModal note={{}}></NoteModal>
+    </div>
   )
 }
 
