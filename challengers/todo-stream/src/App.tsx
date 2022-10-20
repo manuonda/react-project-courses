76f5a70a3b6a8049f129c2868b22ac193f7e7c87
dgarcia <manuonda@gmail.com>
2022-10-19 23:16:47 -0300

    init project todo stream

--- challengers/todo-stream/src/App.tsx
+++ challengers/todo-stream/src/App.tsx
@@ -7,32 +7,39 @@ import React from 'react';
 import noteService from './services/note.service';
 import { NoteCard } from './components/NoteCard';
 
+
+function NoteModal({ onClose }: NoteModalProps) {
+  return (
+    <section 
+    style={{ width: '100vw', height: '100vw', position: 'fixed', top: 0, left: 0 }}
+      className="nes-dialog" id="dialog-default">
+        <div style={{ position:'absolute',
+         display:'flex', alignItems:'center', justifyContent: 'center',
+        backgroundColor: "rgba(0,0,0,0.2)" , width:'100%', height:'100%'}} />
+      <form method="dialog" style={{backgroundColor: "white" , zIndex:1, padding:12 ,border:'5px solid black'}}>
+        <p className="title">Dialog</p>
+        <p>Alert: this is a dialog.</p>
+        <menu className="dialog-menu">
+          <button className="nes-btn" onClick={onClose}>Cancel</button>
+          <button className="nes-btn is-primary">Confirm</button>
+        </menu>
+      </form>
+    </section>
+
+  )
+}
+
 function App() {
   const [notes, setNotes] = React.useState<Note[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
+  const [dfaft,setDraft] =  React.useState< null | Partial<Note>>(null);
 
 
   type NoteModalProps = {
-    note: Partial<Note>
+    note: Partial<Note>,
+    onClose: VoidFunction
   }
 
-  function NoteModal({ }: NoteModalProps) {
-    return (
-      <section style={{ width: '100vw', height: '100vw', position: 'fixed', top: 0, left: 0 }}
-        className="nes-dialog" id="dialog-default">
-          <div style={{ backgroundColor: "rgba(0,0,0,0.2)"}} />
-        <form method="dialog">
-          <p className="title">Dialog</p>
-          <p>Alert: this is a dialog.</p>
-          <menu className="dialog-menu">
-            <button className="nes-btn">Cancel</button>
-            <button className="nes-btn is-primary">Confirm</button>
-          </menu>
-        </form>
-      </section>
-
-    )
-  }
 
   const handleDelete = (id: Note['id']): void => {
     setNotes((notes: Note[]) => notes.filter((note: Note) => note.id !== id));
@@ -90,7 +97,7 @@ function App() {
             key={note.id} note={note} />
         ))}
       </div>
-      <NoteModal note={{}}></NoteModal>
+       {  dfaft && <NoteModal note={draft} onClose></NoteModal>
     </div>
   )
 }
