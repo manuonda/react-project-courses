f5f0371c11d09aa5795811879d0eed975f7ba2b4
David Garcia <david.garcia@grupomost.com>
2023-03-16 20:57:07 -0300

    init project routes protegidas fatz

---
+++ tutorials/rutas-protegidas-fazt/src/App.tsx
@@ -0,0 +1,59 @@
+import { BrowserRouter, Link, Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+import {Admin, Analitycs, Dashboard, Home, Landing}  from "./pages";
+import { useState } from "react";
+
+const App = (): JSX.Element => {
+  const[user,setUser] = useState<User | null >({
+    id: 0,
+    nombre: ""
+  });
+
+  const login = () => {
+    setUser({
+      id: 12,
+      nombre:'Jhon'
+    });
+  }
+
+  const logout = ()=> {
+    setUser(null);
+  }
+  return (
+    <BrowserRouter>
+            <Navigation />
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}>
+          <Route path="/landing" element={<Landing />}></Route>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/analytics" element={<Analitycs />}/>
+          <Route path="/admin" element={<Admin />}/>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Navigation(){
+  return (
+    <ul>
+      <li>
+        <Link to="/landing">Landing</Link>
+      </li>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="/analytics">Analytics</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default App;
---
+++ tutorials/rutas-protegidas-fazt/src/pages/index.tsx
@@ -0,0 +1,5 @@
+export const Landing = () => <h2>Landing Page(Public)</h2>
+export const Home = () => <h2>HOme Page(private)</h2>
+export const Dashboard = () => <h2>Dashboard Page(Private)</h2>
+export const Analitycs = () => <h2>Landing Page(Private permissions : 'analyze')</h2>
+export const Admin = () => <h2>Admin(Private permission 'admin')</h2>
