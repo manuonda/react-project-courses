9af7cde57369314917bb4d1a226db2e122457d6c
David Garcia <david.garcia@grupomost.com>
2024-01-18 20:54:51 -0300

    First Next Project

--- next-projects/nextjs-blog/components/ActiveLink.jsx
+++
@@ -1,7 +0,0 @@
-import Link from 'next/link';
-
-export const ActiveLink = () => {
-    return (
-        <a>Custom Active</a>
-    )
-}
--- next-projects/nextjs-blog/pages/product/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Product {
-    id: string;
-    title: string;
-    category: string;
-    description: string;
-    price: number;
-    image: string;
-}
